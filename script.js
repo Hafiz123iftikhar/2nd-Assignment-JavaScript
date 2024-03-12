a4403fa3469ef5d592f96eedd11babac625b7116
Hafiz123iftikhar <155850590+Hafiz123iftikhar@users.noreply.github.com>
2024-03-12 10:59:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Enter The First Number:");
+let num2 = prompt("Enter The Second Number:");
+let num3 = prompt("Enter The Third Number:");
+
+let sum = Number(num1) + Number(num2) + Number(num3);
+console.log("The sum of the three number is: "+ sum);
